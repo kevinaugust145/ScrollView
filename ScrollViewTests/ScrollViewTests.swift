9a6db005c88d27ace24ce16e1621344e13e563c3
KevinHung <kevinaugust145@gmail.com>
2024-07-14 08:38:11 +0800

    Initial Commit

---
+++ ScrollViewTests/ScrollViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTests.swift
+//  ScrollViewTests
+//
+//  Created by 洪宗燦 on 2024/7/14.
+//
+
+import Testing
+@testable import ScrollView
+
+struct ScrollViewTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
